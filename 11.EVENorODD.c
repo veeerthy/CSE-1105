310d3b9cfb074386f5b110dbf39744e6e5cb9df5
Tanveer Hasan Sifat <tanveerhasan0703@gmail.com>
2025-03-21 16:22:07 +0600

    Assignment 1

---
+++ 11.EVENorODD.c
@@ -0,0 +1,12 @@
+//ID: ET251066
+#include<stdio.h>
+
+int main(){
+    printf("Enter a Number: ");
+    int n;
+    scanf("%d",&n);
+    if(n%2==0) printf("%d is Even",n);
+    else printf("%d is Odd",n);
+
+    return 0;
+}
---
+++ 6.AreaAndRadiusOfCircle.c
@@ -0,0 +1,26 @@
+//ID: ET251066
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double pi = 2*acos(0.0);
+    printf("For Radius -> Area Enter 1.\n For Area -> Radius Enter 2.\n");
+    int n;
+    scanf("%d",&n);
+    if(n==1){
+        double a;
+        printf("Enter Radius:\n");
+        scanf("%lf",&a);
+        double area = pi*a*a;
+        printf("Area = %.2lf\n",area);
+    }
+    else {
+        double a;
+        printf("Enter Area of a Circle:\n");
+        scanf("%lf",&a);
+        double radius = sqrt(a/pi);
+        printf("Radius = %.2lf\n",radius);
+    }
+
+    return 0;
+}
